c0fb41681fb04a60d3d31ff4da0f909f1cc5058c
vinush-vigneswaran <vinush10@hotmail.co.uk>
2024-04-11 22:23:34 +0100

    first django app

---
+++ peek/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.conf import settings
+from django.conf import settings
+
+
+# def peek(request):
+#   template = loader.get_template('video_tiles.html')
+#   return HttpResponse(template.render())
+
+import os
+
+def peek(request):
+# def list_videos(request):
+    videos_dir = os.path.join(settings.MEDIA_ROOT)  # Assuming videos are stored in /media/videos/
+    video_files = [f for f in os.listdir(videos_dir) if f.endswith(('.mp4', '.avi', '.mov'))]
+    # Generate the relative path from MEDIA_ROOT for each video
+    video_urls = ['media/' + file for file in video_files]  # Prepends the 'videos/' directory
+    return render(request, 'video_list.html', {'video_files': video_urls})
